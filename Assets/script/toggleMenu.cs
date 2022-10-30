655fcb698fa85d0ba170a0352f62412214a0163b
theYounger <sheraz@sheraz.ca>
2022-10-30 18:32:17 -0400

    controller highlighter and lens flare

--- Assets/script/toggleMenu.cs
+++ Assets/script/toggleMenu.cs
@@ -5,41 +5,77 @@ using UnityEngine;
 public class toggleMenu : MonoBehaviour
 {
     [SerializeField] GameObject canvas;
+    [SerializeField] GameObject layDownBtn;
+    [SerializeField] GameObject sitDownBtn;
+    public GameObject W_btn;
+    public GameObject W_opacity_btn;
+    public GameObject A_btn;
+    public GameObject A_opacity_btn;
+    public GameObject S_btn;
+    public GameObject S_opacity_btn;
+    public GameObject D_btn;
+    public GameObject D_opacity_btn;
+
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if ((Input.GetKeyDown(KeyCode.W)) || (Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.S)) || (Input.GetKeyDown(KeyCode.D)))
         {
-            canvas.SetActive(false);
+            layDownBtn.SetActive(false);
+            sitDownBtn.SetActive(false);
         }
-        if (Input.GetKeyUp(KeyCode.W))
+        else if ((Input.GetKeyUp(KeyCode.W)) || (Input.GetKeyUp(KeyCode.A)) || (Input.GetKeyUp(KeyCode.S)) || (Input.GetKeyUp(KeyCode.D)))
         {
-            canvas.SetActive(true);
+            layDownBtn.SetActive(true);
+            sitDownBtn.SetActive(true);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if ((Input.GetKeyDown(KeyCode.W)))
         {
-            canvas.SetActive(false);
+            W_btn.SetActive(true);
+            W_opacity_btn.SetActive(false);
         }
-        if (Input.GetKeyUp(KeyCode.S))
+        else if ((Input.GetKeyUp(KeyCode.W)))
         {
-            canvas.SetActive(true);
+            W_btn.SetActive(false);
+            W_opacity_btn.SetActive(true);
+
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if ((Input.GetKeyDown(KeyCode.A)))
         {
-            canvas.SetActive(false);
+            A_btn.SetActive(true);
+            A_opacity_btn.SetActive(false);
         }
-        if (Input.GetKeyUp(KeyCode.A))
+        else if ((Input.GetKeyUp(KeyCode.A)))
         {
-            canvas.SetActive(true);
+            A_btn.SetActive(false);
+            A_opacity_btn.SetActive(true);
+
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if ((Input.GetKeyDown(KeyCode.S)))
         {
-            canvas.SetActive(false);
+            S_btn.SetActive(true);
+            S_opacity_btn.SetActive(false);
         }
-        if (Input.GetKeyUp(KeyCode.D))
+        else if ((Input.GetKeyUp(KeyCode.S)))
         {
-            canvas.SetActive(true);
+            S_btn.SetActive(false);
+            S_opacity_btn.SetActive(true);
+
         }
+        if ((Input.GetKeyDown(KeyCode.D)))
+        {
+            D_btn.SetActive(true);
+            D_opacity_btn.SetActive(false);
+        }
+        else if ((Input.GetKeyUp(KeyCode.D)))
+        {
+            D_btn.SetActive(false);
+            D_opacity_btn.SetActive(true);
+
+        }
+
+
     }
 
 }
